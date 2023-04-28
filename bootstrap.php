54d1cc7d07f396d1e8761994f644732c3d11bcb1
Robert Vogel <1201528+osnard@users.noreply.github.com>
2023-04-28 17:04:58 +0200

    Bump version numbers

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,7 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_COMMONWEBAPIS_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_COMMONWEBAPIS_VERSION', '1.0.21' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_COMMONWEBAPIS_VERSION', '2.0.0' );
 
 MWStake\MediaWiki\ComponentLoader\Bootstrapper::getInstance()
 	->register( 'commonwebapis', function () {
