8cc872f23f11f3db3ae8681ab94a2d7787b61864
wisemoon <willyedoo@gmail.com>
2024-09-27 16:50:45 +0200

    Update mws_category_index_patch_page_title.sql

--- sql/mws_category_index_patch_page_title.sql
+++ sql/mws_category_index_patch_page_title.sql
@@ -1,2 +1,3 @@
-ALTER TABLE `mws_category_index` ADD `mci_page_title` VARBINARY(255) NOT NULL DEFAULT '' AFTER `mci_title`;
-ALTER TABLE `mws_category_index` DROP PRIMARY KEY;
+ALTER TABLE mws_category_index 
+DROP CONSTRAINT IF EXISTS mws_category_index_pkey, 
+DROP PRIMARY KEY;
