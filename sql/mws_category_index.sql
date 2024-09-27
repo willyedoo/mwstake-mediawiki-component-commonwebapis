de3bf62eb4fc8e06bc435ad202538ecec897f807
wisemoon <willyedoo@gmail.com>
2024-09-27 16:49:14 +0200

    Update mws_category_index.sql
    postgresql

--- sql/mws_category_index.sql
+++ sql/mws_category_index.sql
@@ -1,6 +1,6 @@
 CREATE TABLE /*$wgDBprefix*/mws_category_index (
-    mci_cat_id int unsigned NOT NULL,
-	mci_title VARCHAR(255) NOT NULL,
-    mci_page_title VARBINARY(255) NOT NULL DEFAULT '',
-    mci_count int unsigned NOT NULL DEFAULT 0
+    mci_cat_id INT UNSIGNED NOT NULL,
+    mci_title VARCHAR(255) NOT NULL,
+    mci_page_title BYTEA NOT NULL DEFAULT '',
+    mci_count INT UNSIGNED NOT NULL DEFAULT 0
 ) /*$wgDBTableOptions*/;
